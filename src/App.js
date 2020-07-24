ecac58788d436b6a1f911bfb635e2ca37e5207ca
amin <aminebouzrati@gmai.com>
2020-07-24 19:25:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form action="/action_page.php">
+    <div className="form-group">
+      <h1>My Name is James Bond</h1>
+      <label for="email">Email:</label>
+      <input type="email" className="form-control" id="email" placeholder="Enter email" name="email"/>
+    </div>
+    <div className="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd"/>
+    </div>
+    <div className="checkbox">
+      <label><input type="checkbox" name="remember"/> Remember me</label>
+    </div>
+    <button type="submit" className="btn btn-default">Submit</button>
+  </form>
     </div>
   );
 }
